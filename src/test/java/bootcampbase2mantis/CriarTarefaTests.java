e2425f3ced144f22eabe9c9d63e462bec1b95259
pedroeva <pedrodzprogramacao@gmail.com>
2024-06-08 16:54:21 -0300

    08/06/2024 16:54

--- src/test/java/bootcampbase2mantis/CriarTarefaTests.java
+++ src/test/java/bootcampbase2mantis/CriarTarefaTests.java
@@ -23,7 +23,7 @@ public class CriarTarefaTests {
 
         //Busca campo de usuario e insere username
         WebElement usernameField = webDriverWait.until(
-                ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='username']"))
+                ExpectedConditions.elementToBeClickable(By.id("username"))
         );
         usernameField.sendKeys(username);
 
@@ -33,7 +33,7 @@ public class CriarTarefaTests {
 
         //Busca campo de senha e insere senha
         WebElement passwordField = webDriverWait.until(
-                ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='password']"))
+                ExpectedConditions.elementToBeClickable(By.id("password"))
         );
         passwordField.sendKeys(senha);
 
@@ -42,7 +42,7 @@ public class CriarTarefaTests {
         loginButton.click();
     }
 
-    public void criaTarefa(){
+    public void criaTarefa(String reprodubilidade, String prioridade, String resumo, String descricao, String passoAPasso, String informacaoAdicional ){
         //Busca botão de "Reporta issue" e clica
         WebElement criaTarefaBotao = webDriverWait.until(
                 ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='sidebar']/ul/li[3]"))
@@ -51,10 +51,43 @@ public class CriarTarefaTests {
 
         //Busca elemento de categoria e seleciona opção 1 (categoria teste)
         WebElement categoryElement = webDriverWait.until(
-                ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='category_id']"))
+                ExpectedConditions.elementToBeClickable(By.id("category_id"))
         );
         Select categorySelect = new Select(categoryElement);
         categorySelect.selectByValue("1");
+
+
+        //Busca elemento de reproducibilidade e seleciona opção "random"(50)
+
+        WebElement reprodutibilidadeElement = driver.findElement(By.id("reproducibility"));
+        Select reprodutibilidadeSelect = new Select(reprodutibilidadeElement);
+        reprodutibilidadeSelect.selectByVisibleText(reprodubilidade);
+
+        //Busca elemento de prioridade e seleciona opção "low"(20)
+
+        WebElement prioridadeElement = driver.findElement(By.id("priority"));
+        Select prioridadeSelect = new Select(prioridadeElement);
+        prioridadeSelect.selectByVisibleText(prioridade);
+
+        //Busca área de texto de Resumo e insere resumo
+
+        WebElement resumoTextArea = driver.findElement(By.id("summary"));
+        resumoTextArea.sendKeys(resumo);
+
+        //Busca área de texto de Descrição e insere descrição
+
+        WebElement descricaoTextArea = driver.findElement(By.id("description"));
+        descricaoTextArea.sendKeys(descricao);
+
+        //Busca área de texto de passos para reproduzir insere resumo passo a passo
+
+        WebElement passosTextArea = driver.findElement(By.id("steps_to_reproduce"));
+        passosTextArea.sendKeys(passoAPasso);
+
+        //Busca área de texto de Informação adicional e insere informação
+
+        WebElement infoAddTextArea = driver.findElement(By.id("additional_info"));
+        infoAddTextArea.sendKeys(informacaoAdicional);
 
         // Obtém o diretório de trabalho atual e busca a imagem de erro
         Path currentDir = Paths.get(System.getProperty("user.dir"));
@@ -64,38 +97,6 @@ public class CriarTarefaTests {
         // Encontra elemento input file com css selector, pois o sistema usa dropzone.js e o elemento não está visível no DOM
         WebElement inputFile = driver.findElement(By.cssSelector("input[type=file]"));
         inputFile.sendKeys(erroImage);
-
-        //Busca elemento de reproducibilidade e seleciona opção "random"(50)
-        WebElement reprodutibilidadeElement = driver.findElement(By.xpath("//*[@id='reproducibility']"));
-        Select reprodutibilidadeSelect = new Select(reprodutibilidadeElement);
-        reprodutibilidadeSelect.selectByValue("50");
-
-        //Busca elemento de prioridade e seleciona opção "low"(20)
-        WebElement prioridadeElement = driver.findElement(By.xpath("//*[@id='priority']"));
-        Select prioridadeSelect = new Select(prioridadeElement);
-        prioridadeSelect.selectByValue("20");
-
-        //Busca área de texto de Resumo e insere resumo
-        WebElement resumoTextArea = driver.findElement(By.xpath("//*[@id='summary']"));
-        resumoTextArea.sendKeys("Resumo para reporte de um problema com anexo");
-
-        //Busca área de texto de Descrição e insere descrição
-        WebElement descricaoTextArea = driver.findElement(By.xpath("//*[@id='description']"));
-        descricaoTextArea.sendKeys("Teste de descrição para reporte de um problema");
-
-        //Busca área de texto de passos para reproduzir insere resumo passo a passo
-        WebElement passosTextArea = driver.findElement(By.xpath("//*[@id='steps_to_reproduce']"));
-        passosTextArea.sendKeys("" +
-                "1. Acessar o Mantis (https://mantis-prova.base2.com.br/)\n" +
-                "2. Efetuar login (usuario: grupoXX, senha 123456, onde XX é o número do seu grupo)\n" +
-                "3. Clicar em “Criar tarefa” no menu lateral\n" +
-                "4. Selecionar uma categoria\n" +
-                "5. Selecionar uma frequência");
-
-        //Busca área de texto de Informação adicional e insere informação
-        WebElement infoAddTextArea = driver.findElement(By.xpath("//*[@id='additional_info']"));
-        infoAddTextArea.sendKeys("Informação adicional para reporte de um problema");
-
 
         //Busca botão de Submeter Problema e clica
         WebElement submitButton = driver.findElement(By.xpath("//*[@id='report_bug_form']/div/div[2]/div[2]/input"));
@@ -118,15 +119,61 @@ public class CriarTarefaTests {
 
     }
 
+    public boolean validarDadosCriados(String reprodubilidade, String prioridade, String resumo, String descricao, String passoAPasso, String informacaoAdicional){
+        //pegar a informação dentro do campo: Frequência
+        WebElement reprodubilidadeElement = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-container\"]/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div/table/tbody/tr[6]/td[3]")));
+        String reprodubilidadeElementText = reprodubilidadeElement.getText();
+
+        //pegar a informação dentro do campo: Prioridade
+        WebElement prioridadeElement = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-container\"]/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div/table/tbody/tr[6]/td[1]")));
+        String prioridadeElementText = prioridadeElement.getText();
+
+        //pegar a informação dentro do campo: Resumo
+        WebElement resumoElement = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-container\"]/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div/table/tbody/tr[10]/td")));
+        String resumoElementText = resumoElement.getText();
+
+        //pegar a informação dentro do campo: Descrição
+        WebElement descricaoElement = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-container\"]/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div/table/tbody/tr[11]/td")));
+        String descricaoElementText = descricaoElement.getText();
+
+        //pegar a informação dentro do campo: Passo para Reproduzir
+        WebElement passoAPassoElement = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-container\"]/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div/table/tbody/tr[12]/td")));
+        String passoAPassoElementText = passoAPassoElement.getText();
+
+        //pegar a informação dentro do campo: Informação Adicionais
+        WebElement informacaoAdicionalElement =  webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-container\"]/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div/table/tbody/tr[13]/td")));
+        String informacaoAdicionalElementText = informacaoAdicionalElement.getText();
+
+        //Comparar informação extraido com informação esperado pelo parametro
+        boolean statusValidade = reprodubilidadeElementText.equals(reprodubilidade) && prioridadeElementText.equals(prioridade) && resumoElementText.contains(resumo) && descricaoElementText.contains(descricao) && passoAPassoElementText.equals(passoAPasso) && informacaoAdicionalElementText.contains(informacaoAdicional);
+
+        return statusValidade;
+
+    }
+
     @Test
     public void criarTarefaComSucesso(){
         driver.manage().window().maximize();
         login("grupoVerde2", "123456");
-        criaTarefa();
+        String reprodubilidade ="aleatório";
+        String prioridade = "baixa";
+        String resumo = "Resumo para reporte de um problema com anexo";
+        String descricao = "Teste de descrição para reporte de um problema";
+        String passoAPasso =  "" +
+                "1. Acessar o Mantis (https://mantis-prova.base2.com.br/)\n" +
+                "2. Efetuar login (usuario: grupoXX, senha 123456, onde XX é o número do seu grupo)\n" +
+                "3. Clicar em “Criar tarefa” no menu lateral\n" +
+                "4. Selecionar uma categoria\n" +
+                "5. Selecionar uma frequência";
+        String informacaoAdicional = "Informação adicional para reporte de um problema";
+
+        criaTarefa(reprodubilidade, prioridade, resumo, descricao, passoAPasso, informacaoAdicional);
         boolean statusCriacao = foiCriado();
+        boolean statusDados = validarDadosCriados(reprodubilidade, prioridade, resumo, descricao, passoAPasso, informacaoAdicional);
 
         driver.quit();
 
         Assert.assertTrue(statusCriacao);
+        Assert.assertTrue(statusDados);
     }
 }
